f997520dbed5a520f17a70d176706ea14946f55d
P4r4dis <adil.d.pro@gmail.com>
2024-02-28 17:39:25 +0100

    [ADD] - unit test for Lemon class implementation. [UPDATE] - Orange class has been deleted for be replace by Lemon class.

--- Ex0_Fruit/test/bin/test_main.cpp
+++ Ex0_Fruit/test/bin/test_main.cpp
@@ -5,7 +5,7 @@
 #include <criterion/parameterized.h>
 #include <signal.h>
 
-#include "../include/Fruit.hpp"
+#include "../include/Lemon.hpp"
 
 void redirect_all_stdout(void)
 {
@@ -13,26 +13,39 @@ void redirect_all_stdout(void)
     cr_redirect_stderr();
 }
 
-class Orange : public Fruit
-{
-    public:
-        Orange(void) : Fruit()
-        {
-            setName("Orange");
-            setVitamins(10);
-        };
-        ~Orange(void) {};
-};
-
 Test(Fruit, test_Fruit_class_implementation, .init = redirect_all_stdout)
 {
     {
-        Orange orange;
-        Fruit &f = orange;
+        Lemon l;
+        Fruit &f = l;
 
-        cr_assert(f.getName() == "Orange");
-        cr_assert(f.getVitamins() == 10);
+        cr_assert(f.getName() == "lemon");
+        cr_assert(f.getVitamins() == 3);
+        std::cout   << f.getVitamins() << std::endl;
+        std::cout   << f.getName() << std::endl;
     }
 
-    cr_assert_stdout_eq_str("");
+    cr_assert_stdout_eq_str
+    (
+        "3\n"
+        "lemon\n"
+    );
+}
+
+Test(Lemon, test_Lemon_class_implementation, .init = redirect_all_stdout)
+{
+    {
+        Lemon l;
+
+        cr_assert(l.getName() == "lemon");
+        cr_assert(l.getVitamins() == 3);
+        std::cout   << l.getVitamins() << std::endl;
+        std::cout   << l.getName() << std::endl;
+    }
+
+    cr_assert_stdout_eq_str
+    (
+        "3\n"
+        "lemon\n"
+    );
 }
---
+++ Ex0_Fruit/test/include/Lemon.hpp
@@ -0,0 +1,13 @@
+#ifndef             __LEMON_HPP__
+    #define         __LEMON_HPP__
+
+    #include "Fruit.hpp"
+
+    class Lemon : public Fruit
+    {
+        public:
+            Lemon(void);
+            ~Lemon(void);
+};
+
+#endif //           !__LEMON_HPP__S
